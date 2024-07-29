d7a6cf3f6ecce72b0e378a77c6bae256086b5f8d
Tanu <tanupandey291099@gmail.com>
2024-07-29 23:33:42 +0530

    setting things up

---
+++ main.js
@@ -0,0 +1,13 @@
+const myLibrary = [];
+
+function Book() {
+  // the constructor...
+}
+
+function addBookToLibrary() {
+
+    // basically i get data from a form 
+    // i need to push this data in myLibrary
+
+  // do stuff here
+}
