ba12f93d6e5b15e75f9720cf88dab7a260f86a26
Tanu <tanupandey291099@gmail.com>
2024-08-01 15:37:23 +0530

    adding a demo of functionality

--- main.js
+++ main.js
@@ -1,13 +1,54 @@
+// const title = document.querySelector("#title");
+// console.log(title.value);
+const submit = document.querySelector("button");
 const myLibrary = [];
+const container = document.querySelector(".container");
+let count = 0;
 
-function Book() {
+function Book(title) {
+  this.title = title;
   // the constructor...
 }
 
-function addBookToLibrary() {
+function addBookToLibrary(title) {
 
-    // basically i get data from a form 
-    // i need to push this data in myLibrary
+  // basically i get data from a form 
+  // i need to push this data in myLibrary
+
+  const Book1 = new Book(title);
+  myLibrary.push(Book1);
+
+
+  // I need to display it in cards ;
+  const div = document.createElement("div");
+  const button = document.createElement("button");
+  div.className = "card";
+  div.textContent = "title: " + title;
+  button.textContent = "Remove";
+  button.className = "Remove";
+  div.dataset.index = count++;
+  console.log(div.dataset.index);
+  div.appendChild(button);
+  container.appendChild(div);
+
+
+  const remove = document.querySelector(".Remove");
+
+  remove.addEventListener("click", function () {
+    console.log("removed");
+  });
+
+
 
   // do stuff here
 }
+submit.addEventListener("click", function (e) {
+  const title = document.querySelector("#title");
+  addBookToLibrary(title.value);
+  console.log(myLibrary);
+
+  e.preventDefault();
+
+
+
+});
