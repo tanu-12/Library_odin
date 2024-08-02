a8d66c215f425bfa3356be7dce271d73f93fdcc6
Tanu <tanupandey291099@gmail.com>
2024-08-02 16:57:07 +0530

    fixed a few issue

--- main.js
+++ main.js
@@ -9,22 +9,6 @@ function Book(title) {
   this.title = title;
   // the constructor...
 }
-
-function addBookToLibrary(title) {
-
-  // basically i get data from a form 
-  // i need to push this data in myLibrary
-
-  const Book1 = new Book(title);
-  myLibrary.push(Book1);
-
-
-  // I need to display it in cards ;
-  displayCard();
-
-}
-
-
 submit.addEventListener("click", function (e) {
   const title = document.querySelector("#title");
   addBookToLibrary(title.value);
@@ -35,18 +19,38 @@ submit.addEventListener("click", function (e) {
 
 
 });
-function displayCard() {
+function addBookToLibrary(title) {
+
+  // basically i get data from a form 
+  // i need to push this data in myLibrary
+
+  const Book1 = new Book(title);
+  myLibrary.push(Book1);
+
+
+  // I need to display it in cards ;
+  displayCard(title);
+
+}
+function displayCard(title) {
+
+  //we are also creating cards dynamically here so that we can get to display 
   const div = document.createElement("div");
   const button = document.createElement("button");
   div.className = "card";
-  div.textContent = "title: " + myLibrary.title;
+  div.textContent = "title: " + title;
   button.textContent = "Remove";
   button.className = "Remove";
   div.dataset.index = count++;
   console.log(div.dataset.index);
-
-
   div.appendChild(button);
   container.appendChild(div);
+  button.addEventListener("click", function () {
+    console.log("remove is added");
+    console.log(div.dataset.index);
+
+    console.log(myLibrary.splice(div.dataset.index, 1));
+
+  });
 
 }
