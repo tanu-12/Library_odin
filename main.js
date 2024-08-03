ee641cb2cf6502c4e345022f5e8a6c6a48ce64b4
Tanu <tanupandey291099@gmail.com>
2024-08-03 23:40:52 +0530

    added read button

--- main.js
+++ main.js
@@ -3,48 +3,56 @@
 const submit = document.querySelector("button");
 const myLibrary = [];
 const container = document.querySelector(".container");
+var read = document.querySelector("#read");
+
 let count = 0;
 
-function Book(title) {
+function Book(title, author, pages) {
   this.title = title;
+  this.author = author;
+  this.pages = pages;
   // the constructor...
 }
 submit.addEventListener("click", function (e) {
   const title = document.querySelector("#title");
-  const pages = document.query
-  addBookToLibrary(title.value);
+  const author = document.querySelector("#author");
+  const pages = document.querySelector("#pages");
+  addBookToLibrary(title.value, author.value, pages.value);
   console.log(myLibrary);
+
 
   e.preventDefault();
 
 
 
 });
-function addBookToLibrary(title) {
+function addBookToLibrary(title, author, pages) {
 
   // basically i get data from a form 
   // i need to push this data in myLibrary
 
-  const Book1 = new Book(title);
+  const Book1 = new Book(title, author, pages);
   myLibrary.push(Book1);
 
 
   // I need to display it in cards ;
-  displayCard(title);
+  displayCard(title, author, pages);
 
 }
-function displayCard(title) {
+function displayCard(title, author, pages) {
 
   //we are also creating cards dynamically here so that we can get to display it
   const div = document.createElement("div");
   console.log(div.textContent);
   const name = document.createElement("div");
-  const pages = document.createElement("div");
-  const read = document.createElement("div");
+  const page = document.createElement("div");
+  const Author = document.createElement("div");
 
   const button = document.createElement("button");
   div.className = "card";
   name.textContent = title;
+  Author.textContent = author;
+  page.textContent = pages;
   name.className = "title"
   console.log(name.textContent);
   // button.textContent = "Remove";
@@ -52,9 +60,9 @@ function displayCard(title) {
   div.dataset.index = myLibrary.findIndex(x => x.title == title);
   console.log(div.dataset.index);
 
-  div.append(name, button)
+  div.append(name, Author, page, button)
   container.appendChild(div);
-  console.log(document.querySelectorAll(".card")[0].textContent);
+  // console.log(document.querySelectorAll(".card")[0].textContent);
   button.addEventListener("click", function (event) {
     console.log("remove is added");
     console.log(event.target.parentNode.getAttribute('data-index'));
@@ -66,6 +74,45 @@ function displayCard(title) {
     div.parentNode.removeChild(div);
     console.log(myLibrary);
     updateIndex(document.querySelectorAll(".title"));
+
+
+
+  });
+
+
+  var readBtn = document.createElement("button");
+  if (read.checked) {
+    readBtn.style.backgroundColor = "green";
+    readBtn.textContent = "Read";
+
+  }
+  else {
+    readBtn.style.backgroundColor = "red";
+    readBtn.textContent = "Not Read";
+  }
+  readBtn.value = read.checked;
+  console.log(readBtn.value);
+
+
+
+  div.append(readBtn);
+
+
+  readBtn.addEventListener("click", function (e) {
+    if (e.target.value == "false") {
+      console.log("checking");
+
+      e.target.style.backgroundColor = "green";
+      readBtn.textContent = "Read";
+      e.target.value = "true";
+    }
+    else if (e.target.value) {
+
+      readBtn.style.backgroundColor = "red";
+      readBtn.textContent = "Not Read";
+      e.target.value = "false";
+    }
+
 
 
 
